f355312728102f7b2977351314ff3c47470e1ec7
Ephraim Jan Conales <conalesej@gmail.com>
2023-10-01 00:26:14 +0800

    added pricerange component

---
+++ client-app/src/app/landing/components/Filter/components/PriceRange/PriceRange.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./PriceRange.scss";
+interface IPriceRange {}
+
+const PriceRange: React.FC<IPriceRange> = () => {
+  return <>PriceRange</>;
+};
+
+export default PriceRange;
