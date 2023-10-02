bff83aea8b4f535b8fe9ab7889055505306631bd
Ephraim Jan Conales <conalesej@gmail.com>
2023-10-02 23:12:41 +0800

    add product list to index

--- client-app/src/app/landing/components/index.ts
+++ client-app/src/app/landing/components/index.ts
@@ -2,5 +2,6 @@ import Carousel from "./Carousel/Carousel";
 import SearchBar from "./Search/SearchBar";
 import Categories from "./Categories/Categories";
 import FilterBar from "./Filter/FilterBar";
+import ProductList from "./Products/ProductList";
 
-export { Carousel, SearchBar, Categories, FilterBar };
+export { Carousel, SearchBar, Categories, FilterBar, ProductList };
