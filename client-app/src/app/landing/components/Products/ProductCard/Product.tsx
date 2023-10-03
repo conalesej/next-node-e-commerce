255b691621d0ce71bc3df9a8ec871f0d4f5ad629
Ephraim Jan Conales <conalesej@gmail.com>
2023-10-03 23:58:06 +0800

    added picture to product

--- client-app/src/app/landing/components/Products/ProductCard/Product.tsx
+++ client-app/src/app/landing/components/Products/ProductCard/Product.tsx
@@ -1,12 +1,23 @@
 import React from "react";
-import "./Product.scss"
+import "./Product.scss";
 import { ICar } from "@/app/utils/types";
 
 interface IProduct {
   product: ICar;
 }
 const Product: React.FC<IProduct> = ({ product }) => {
-  return <div className="Product">{product.make}</div>;
+  return (
+    <div className="Product">
+      <div className="image">
+        <img
+          src="https://source.unsplash.com/random/?Cars&1"
+          alt="https://unsplash.it"
+          className="image-holder"
+        />
+      </div>
+      <div className="description"></div>
+    </div>
+  );
 };
 
 export default Product;
