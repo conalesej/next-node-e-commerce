69a2caa8841863d3c4d538732e9560cddd247fe5
Ephraim Jan Conales <conalesej@gmail.com>
2023-10-04 14:36:41 +0800

    Product - add product description

--- client-app/src/app/landing/components/Products/ProductCard/Product.tsx
+++ client-app/src/app/landing/components/Products/ProductCard/Product.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import "./Product.scss";
+
 import { ICar } from "@/app/utils/types";
 
+import SpeedIcon from "@mui/icons-material/Speed";
+import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
+import { formatNumberWithCommas } from "@/app/utils/util";
 interface IProduct {
   product: ICar;
 }
@@ -14,8 +18,31 @@ const Product: React.FC<IProduct> = ({ product }) => {
           alt="https://unsplash.it"
           className="image-holder"
         />
+        <div className="body-type">{product.body_type}</div>
       </div>
-      <div className="description"></div>
+      <div className="description">
+        <div className="desc-header">
+          <span>{product.year}</span>
+          {" • "}
+          <span>{product.fuel_type}</span>
+          {" • "}
+          <span>{product.transmission}</span>
+        </div>
+        <div className="desc-make-model">
+          {product.make + " " + product.model}
+        </div>
+        <div className="desc-mileage">
+          <SpeedIcon htmlColor="gray" style={{ marginRight: 5 }} />
+          {product.mileage + " KM"}
+        </div>
+        <div className="desc-price">
+          <MonetizationOnOutlinedIcon
+            htmlColor="orange"
+            style={{ marginRight: 5 }}
+          />
+          {formatNumberWithCommas(product.price)}
+        </div>
+      </div>
     </div>
   );
 };
