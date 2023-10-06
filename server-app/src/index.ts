025b3e4b30e87612763adf528e1621f8f4b1f386
Ephraim Jan Conales <conalesej@gmail.com>
2023-10-06 21:47:11 +0800

    Index - updated to add productRouter route

--- server-app/src/index.ts
+++ server-app/src/index.ts
@@ -1,9 +1,23 @@
+import { productRouter } from "./routes";
+
 const express = require("express");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const PORT = 3001;
 
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cookieParser());
 
 app.listen(PORT, () => {
-    console.log(`Running Express server on port ${PORT}`);
-  });
+  console.log(`Running Express server on port ${PORT}`);
+});
+
+// Print routes
+app.use((req: Request, _: Response, next: () => void) => {
+  console.log(`Method: ${req.method}, Route: ${req.url}`);
+  next();
+});
+
+app.use("/product", productRouter);
